900193b3cb7325889244d16f1ac75a60a153a6b2
jonathanmunamire <jonathanmunamire@gmail.com>
2022-11-19 10:20:12 +0200

    Set Up Project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,81 @@
 import _ from 'lodash';
 import './style.css';
 
+const countriesContainer = document.querySelector(".country_section");
+const search = document.querySelector(".search_button");
+const input = document.querySelector(".input");
+const message = document.querySelector(".message");
+let countryName = "greece"
 
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+search.addEventListener('click', ()=>{
+  if (input.value = '') {
+    message.innerHTML = `Add Country`
+  }else {
+    countryName = "portugal";
+    message.innerHTML = countryName;
+    console.log(countryName);
   }
-  
-  document.body.appendChild(component());
+})
+
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v2/name/${countryName}`);
+request.send();
+
+request.addEventListener('load', ()=> {
+  const [data] = JSON.parse(request.responseText);
+  console.log(data);
+
+  countriesContainer.innerHTML = `<article class="country_card">
+  <img
+    class="country_image"
+    src="${data.flag}"
+    alt=""
+  />
+  <div class="country_data">
+    <h2 class="country_name">${data.name}</h2>
+    <h3 class="country_region">${data.region}</h3>
+    <p>
+      🕔
+      <span> Time-Zone : </span> ${data.timezones[0]}
+    </p>
+    <p>
+      🏢
+      <span> Capital : </span>${data.capital}
+    </p>
+    <p>
+      🗣️
+      <span> Languages : </span> ${data.languages[0].name}
+    </p>
+    <p>
+      📞
+      <span> Code : +</span>${data.callingCodes}
+    </p>
+    <p>
+      🧑🏻‍🤝‍🧑🏾
+      <span> Population : </span> ${data.population}
+    </p>
+    <p>
+      📏
+      <span> Area : </span> ${data.area} <span>km<sup>2</sup></span>
+    </p>
+    <p>
+      🚧
+      <span> Borders : </span> ${[data.borders]}
+    </p>
+    <p>
+      💵
+      <span> Currencies : </span> ${data.currencies[0].name} (${data.currencies[0].symbol})
+    </p>
+    <p>
+      🤝
+      <span> Block : </span> ${data.regionalBlocs[0].name}
+    </p>
+    <p>
+      🌍
+      <span> Lat-Long : </span> (${data.latlng})
+    </p>
+  </div>
+</article>`;
+})
+
+
